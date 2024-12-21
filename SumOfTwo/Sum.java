fdb9c0278e4b36813764c38f2ac9b0496864f94d
Sonna1964 <gaganahsonnadara99@gmail.com>
2024-12-21 13:58:43 +0530

    Basic sum of two

---
+++ SumOfTwo/Sum.java
@@ -0,0 +1,10 @@
+package SumOfTwo;
+
+public class Sum {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 6;
+        int res = a + b;
+        System.out.println(res); 
+    }
+}
