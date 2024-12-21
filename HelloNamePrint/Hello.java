230e917c1670041df456d185c3748681f8afd33a
Sonna1964 <gaganahsonnadara99@gmail.com>
2024-12-21 15:35:18 +0530

    Added comments how the helloworld program works

--- HelloNamePrint/Hello.java
+++ HelloNamePrint/Hello.java
@@ -1,5 +1,6 @@
 public class Hello {
     public static void main(String[] args) {
+        //print the Hello message to console
         System.out.println("Hello\nGagana H. Sonnadara...!!");
     }
 }
--- HelloNamePrint/Hello2.java
+++ HelloNamePrint/Hello2.java
@@ -2,11 +2,15 @@ import java.util.Scanner;
 
 public class Hello2 {
     public static void main(String[] args) {
+        //create the Scanner Object to read input from console
         Scanner input = new Scanner(System.in);
+        //tell user to input the first number in to the console
         System.out.print("Input your first name : ");
         String fname = input.next();
+        //tell user to input the second number in to the console
         System.out.print("Input your last name : ");
         String lname = input.next();
+        //concatinante the 2 names and output to the console as a greeting msg
         System.out.println("Your name is : " + fname + " " + lname);
     }
 }
